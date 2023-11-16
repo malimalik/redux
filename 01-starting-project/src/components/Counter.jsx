be09fb89e21407fa71822f7af7b7265c0399ee5d
Ali <ialimalik99@gmail.com>
2023-11-16 14:34:23 -0500

    add increment by 10 function

---
+++ 01-starting-project/src/components/Counter.jsx
@@ -0,0 +1,41 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const toggleCounterHandler = () => {};
+
+  const dispatch = useDispatch();
+  const result = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({
+      type: "increment",
+    });
+  };
+
+  const increaseHandler = () => {
+    dispatch({
+      type: 'increase', 
+      amount: 10,
+    })
+  }
+  const decrementHandler = () => {
+    dispatch({
+      type: "decrement",
+    });
+  };
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{result}</div>
+      <div>
+        <button type="button" onClick={incrementHandler}>Increment</button>
+        <button type="button" onClick={decrementHandler}>Decrement</button>
+        <button type="button" onClick={increaseHandler}>Increase by 10</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
